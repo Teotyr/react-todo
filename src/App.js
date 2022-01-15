db3f90e80730c433ea6ba4b760cec0c55051875b
Teotyr <dogukantahaydin@gmail.com>
2022-01-15 03:10:07 +0300

    I added bold

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <b>Bold</b>
+        <as
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </as>
       </header>
     </div>
   );
